6581fa6a91fdb6cf18f0ce8c0a6a7d56bb3950d2
hiei23 <yuikazuga_25@hotmail.co.uk>
2017-03-28 16:00:22 -0400

    added test script

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+INPUT_FILES=(
+	"books.json" 
+	"records.json" 
+	"friends.json" 
+	"dna.json"
+	)
+
+FILES=(
+	"inverted_index" 
+	"join_result" 
+	"friend_count" 
+	"unique_trims"
+	)
+
+# get length of an array
+arraylength=${#INPUT_FILES[@]}
+
+mkdir results
+printf "\n>>>>>>>>>>>>> Testing Files <<<<<<<<<<<<<\n\n"
+
+for (( i=1; i < ${arraylength} + 1; i++ ));
+do
+  	echo "Testing: " ${PROGRAMS[$i-1]}
+  	python "${FILES[$i-1]}.py" "input/${INPUT_FILES[$i-1]}" > "results/${FILES[$i-1]}.json"
+  
+  	printf "\n>>>>>>>>>>>>> Checking Difference <<<<<<<<<<<<<\n\n"
+  	diff "solutions/${FILES[$i-1]}.json" "results/${FILES[$i-1]}.json" > "${FILES[$i-1]}_diff.txt"
+done
